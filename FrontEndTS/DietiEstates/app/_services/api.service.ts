cc48bc959cf9bf709b6f984b9e087e1920a0932d
Fab <fabrizioapuzzo2003@gmail.com>
2025-08-15 18:32:36 +0200

    search fix

--- FrontEndTS/DietiEstates/app/_services/api.service.ts
+++ FrontEndTS/DietiEstates/app/_services/api.service.ts
@@ -54,7 +54,7 @@ export const apiEndpoints = {
   agentProperties: '/agent/properties', // Endpoint per lista immobili agente
 
   //? Properties (Buyer & General)
-  searchProperties: '/properties/search', // Endpoint per ricerca immobili
+  searchProperties: '/properties/search/', // Endpoint per ricerca immobili
   featuredProperties: '/properties/featured', // Endpoint per immobili in evidenza
   propertyDetails: '/properties/details', // Endpoint base per dettagli immobile (si userà /properties/{id})
   createProperty: '/properties/create', // Endpoint per creare un nuovo immobile (POST)
@@ -362,7 +362,9 @@ export const searchProperties = async (
   console.warn("searchProperties: La logica API reale non è completamente implementata per i filtri complessi.");
   // Fallback a una chiamata semplice per ora se non si usa il mock
   const response = await httpClient.get(apiEndpoints.searchProperties+params.query, { params: { query: params.query } }); // TODO rimuovere parametri (params), utili solo per MOCK
-  return response.data;
+  const DTOs: PropertyDTO[] = response.data;
+  const ret = await Promise.all(DTOs.map((value: PropertyDTO) => PropertyDTO_to_PropertyDetail(value)));
+  return ret;
 };
 
 /**
